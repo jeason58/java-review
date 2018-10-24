9cb05bea8fc3f53373149e0bb765f6621ab491ff
wangzhixin <wangzhixin@mohuoer.com>
2018-10-24 16:45:40 +0800

    add some practice like ClassLoaderPractice and interview package

--- src/main/java/com/jeason/practice/study08/OOMCreateTest.java
+++ src/main/java/com/jeason/practice/study08/OOMCreateTest.java
@@ -6,13 +6,13 @@ import java.util.List;
 
 /**
  * @description: 减小堆内存参数设置，模拟产生堆内存溢出
- * @VmArgs：-Xmx2m -Xms2m -XX:HeapDumpOnOutOfMemoryError
+ * @VmArgs：-Xmx4m -Xms4m -Xmn2m -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=D:\\oomCreateTest.dump
  * @author: jeason·wang
  * @date: 2018-08-02 12:45
  **/
 public class OOMCreateTest {
   static class OOMObject {
-
+    String field = "dgziorghzrog;srog'ShgsoghsighsirghS:Ighsriug";
   }
 
   public static void main(String[] args) {
