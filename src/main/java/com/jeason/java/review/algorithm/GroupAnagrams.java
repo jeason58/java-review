e9434ddd286dda0dac3578c8bdbaaf7549fdf35c
wangzhixin <wangzhixin@megvii.com>
2020-04-17 13:08:57 +0800

    GroupAnagrams updated

--- src/main/java/com/jeason/java/review/algorithm/GroupAnagrams.java
+++ src/main/java/com/jeason/java/review/algorithm/GroupAnagrams.java
@@ -1,25 +1,40 @@
 package com.jeason.java.review.algorithm;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class GroupAnagrams {
-    public static List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List> group = new HashMap<>();
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> groups = new HashMap<>();
 
         for (String word : strs) {
+            String key = this.sortStrChar(word.toCharArray());
+            List<String> group = groups.get(key);
+            if (group == null) {
+                group = new ArrayList<String>();
 
+            }
+
+            group.add(word);
+            groups.put(key, group);
         }
 
-        return null;
+        List<List<String>> wordGroups = new ArrayList<>();
+        for (String k : groups.keySet()) {
+            wordGroups.add(groups.get(k));
+        }
+
+        return wordGroups;
     }
 
-    public static String sortStrChar(char[] chs) {
-        for (int i = 0; i < chs.length; i--) {
+    public String sortStrChar(char[] chs) {
+        char tmp;
+        for (int i = 0; i < chs.length-1; i++) {
             for (int j = 0; j < chs.length-i-1; j++) {
                 if (chs[j+1] < chs[j]) {
-                    char tmp = chs[j];
+                    tmp = chs[j];
                     chs[j] = chs[j+1];
                     chs[j+1] = tmp;
                 }
@@ -29,31 +44,12 @@ public class GroupAnagrams {
         return String.valueOf(chs);
     }
 
-    public static boolean wordExists(String word, String chars) {
-        if ("".equals(word)) {
-            return true;
-        }
-
-        String curC = word.substring(0, 1);
-        String nextWord = "";
-        if (word.length() == 1) {
-            nextWord = "";
-        } else {
-            nextWord = word.substring(1);
-        }
-
-        for (int i = 0; i < chars.length(); i++) {
-            String ch = chars.substring(i, i + 1);
-            if (ch.equals(curC)) {
-                return wordExists(nextWord, chars);
-            }
-        }
-        return false;
-    }
-
     public static void main(String[] args) {
-        String s = "trsab";
-        char[] chs = sortStrChar(s.toCharArray());
-        System.out.println(chs);
+        GroupAnagrams ga = new GroupAnagrams();
+        String[] strs = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> groups = ga.groupAnagrams(strs);
+        for (List<String> g : groups ) {
+            System.out.println(g);
+        }
     }
 }
