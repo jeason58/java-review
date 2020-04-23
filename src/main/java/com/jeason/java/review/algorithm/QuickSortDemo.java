0248d41ef12e16592406a886c9092cd3839afebd
jeason58 <2356914868@qq.com>
2020-04-23 22:44:53 +0800

    QuickSortDemo update

--- src/main/java/com/jeason/java/review/algorithm/QuickSortDemo.java
+++ src/main/java/com/jeason/java/review/algorithm/QuickSortDemo.java
@@ -6,39 +6,34 @@ package com.jeason.java.review.algorithm;
  * @Description: 快速排序算法实现，不稳定，时间复杂度 = O(N*logN)
  */
 public class QuickSortDemo {
-    private static int divide(int[] arr, int start, int end) {
-        int base = arr[start];
-        while (start < end) {
-            while (start < end && arr[end] >= base) {
-                end --;
-            }
-            if (start < end) {
-                int temp = arr[end];
-                arr[end] = arr[start];
-                arr[start] = temp;
-                start ++;
-            }
-
-            while (start < end && arr[start] <= base) {
-                start ++;
-            }
-            if (start < end) {
-                int temp = arr[start];
-                arr[start] = arr[end];
-                arr[end] = temp;
-                end --;
-            }
-        }
-        return start;
-    }
-
-    public static void quickSort(int[] arr, int start, int end) {
-        if (arr == null || start > end || start < 0 || end >= arr.length) {
+    public static void quickSort(int[] arr, int left, int right) {
+        if (arr == null || left > right || left < 0 || right >= arr.length) {
             return;
         }
-        int partition = divide(arr, start, end);
-        quickSort(arr, start, partition-1);
-        quickSort(arr, partition+1, end);
+
+        int base = arr[left];
+        int i = left, j = right;
+        while (i < j) {
+            while (i < j && arr[j] >= base) {
+                j --;
+            }
+
+            while (i < j && arr[i] <= base) {
+                i ++;
+            }
+
+            if (i < j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        arr[left] = arr[i];
+        arr[i] = base;
+
+        quickSort(arr, left, i-1);
+        quickSort(arr, i+1, right);
     }
 
     public static void printArray(int[] array) {
@@ -49,7 +44,7 @@ public class QuickSortDemo {
     }
 
     public static void main(String[] args) {
-        int[] A = {4, 9, 7, 5, 5, 5, 8, 6, 2};
+        int[] A = {6, 1, 2, 7, 9, 3, 4, 5, 10, 8};
         System.out.println("before sort: ");
         printArray(A);
         quickSort(A, 0, A.length-1);
