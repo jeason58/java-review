33140762f4fe077aeefa26cd43159388c7eb9fc2
wangzhixin <wangzhixin@megvii.com>
2020-04-26 18:46:25 +0800

    add LinkedListReverse

---
+++ src/main/java/com/jeason/java/review/algorithm/LinkedListReverse.java
@@ -0,0 +1,64 @@
+package com.jeason.java.review.algorithm;
+
+public class LinkedListReverse {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static ListNode initListNode(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return null;
+        }
+
+        ListNode head = null, cur = null;
+        for (int i=0; i<arr.length; i++) {
+            if ( i == 0) {
+                cur = new ListNode(arr[i]);
+                head = cur;
+            } else {
+                cur.next = new ListNode(arr[i]);
+                cur = cur.next;
+            }
+        }
+
+        return head;
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode pre = null, cur = head, next = cur.next;
+        while (next != null) {
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+            next = next.next;
+        }
+        cur.next = pre;
+
+        return cur;
+    }
+
+    public static void displayList(ListNode head) {
+        while (head != null) {
+            System.out.printf("%2d\t", head.val);
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListNode head = initListNode(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9});
+        displayList(head);
+
+        ListNode revHead = reverseList(head);
+        displayList(revHead);
+    }
+}
--- src/main/java/com/jeason/java/review/algorithm/SortByInsertDemo.java
+++ src/main/java/com/jeason/java/review/algorithm/SortByInsertDemo.java
@@ -10,7 +10,7 @@ public class SortByInsertDemo {
 
     // 思想：类似于抓扑克牌，左手的牌是已排好序的，右手的牌是未排好序的，
     // 将第一张牌作为左手的已经排好序牌，每次从右手取一张牌，然后依次从右往左与左手的牌比较，
-    // 若左手的牌比拿到的这张牌大，则依次右移，知道碰到左手的某张牌X比拿到的这张牌小，便将这张牌插入到X牌的后面
+    // 若左手的牌比拿到的这张牌大，则依次右移，直到碰到左手的某张牌X比拿到的这张牌小，便将这张牌插入到X牌的后面
     // 重复上述过程直至右手的牌全部被插入
     public static void insertionSort(int[] arr) {
         if (arr == null || arr.length < 2) {
@@ -29,7 +29,7 @@ public class SortByInsertDemo {
     }
 
     public static void main(String[] args) {
-        int[] A = {5, 8, 4, 9, 6, 3, 2, 7};
+        int[] A = {5, 8, 4, 9, 6, 3, 2, 7, 1};
         insertionSort(A);
         System.out.println("after sort: ");
         SortByQuickDemo.printArray(A);
