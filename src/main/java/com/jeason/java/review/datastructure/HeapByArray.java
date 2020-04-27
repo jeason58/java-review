e32d351e422ade1387c9705072ee45272d0ddfb0
wangzhixin <wangzhixin@megvii.com>
2020-04-27 18:28:03 +0800

    add HeapByArray

---
+++ src/main/java/com/jeason/java/review/datastructure/HeapByArray.java
@@ -0,0 +1,90 @@
+package com.jeason.java.review.datastructure;
+
+
+import java.util.LinkedList;
+
+/**
+ * 基于数组实现堆结构（大顶堆和小顶堆）
+ * 堆的定义：
+ * 1）堆是一颗完全二叉树；
+ * 2）大顶堆的每个节点都大于等于其左右子节点；
+ * 3）小顶堆的每个节点都小于等于其左右子节点；
+ *
+ * 存储结构：
+ * 因为堆是一颗完全查找树，而完全二叉树最好的存储方案是数组：
+ * 可实现随机访问，能够迅速定位左右子节点和父节点，数组的第一个元素（下标为0）一般不存储节点；
+ *
+ * */
+
+//TODO: 待完善，堆结构不正确
+public class HeapByArray {
+    int[] heapTab;
+    int cap;
+    int count;
+    private static final int DEFAULT_HEAP_CAP = 10;
+
+    public HeapByArray(){
+        this(DEFAULT_HEAP_CAP);
+    }
+
+    public HeapByArray(int cap) {
+        if (cap <= 0) {
+            cap = DEFAULT_HEAP_CAP;
+        }
+        this.heapTab = new int[cap+1];
+        this.cap = cap;
+        this.count = 0;
+    }
+
+    private void resize() {
+        cap = (cap << 1) -1;
+        int[] oldTab = heapTab;
+        heapTab = new int[cap];
+        for (int i = 1; i <= count; i++) {
+            heapTab[i] = oldTab[i];
+        }
+    }
+
+    public int insert(int val) {
+        if (count+1 == cap) {
+            resize();
+        }
+
+        heapTab[++count] = val;
+        heapify(count);
+
+        return count;
+    }
+
+    public int size() {
+        return count;
+    }
+
+    private void heapify(int targetIndex) {
+        int tmp;
+        while (heapTab[targetIndex/2] > 0 && heapTab[targetIndex] > heapTab[targetIndex/2]) {
+            tmp = heapTab[targetIndex];
+            heapTab[targetIndex] = heapTab[targetIndex/2];
+            heapTab[targetIndex/2] = tmp;
+        }
+    }
+
+    public void display() {
+        for (int i = 1; i <= count; i++) {
+            System.out.printf("%2d\t", heapTab[i]);
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+        HeapByArray heap = new HeapByArray(8);
+
+        int[] arr = new int[]{3, 5, 7, 2, 4, 9, 1, 6};
+        for (int n : arr) {
+            heap.insert(n);
+        }
+
+        heap.display();
+    }
+}
---
+++ src/main/java/com/jeason/java/review/datastructure/StackByArray.java
@@ -0,0 +1,57 @@
+package com.jeason.java.review.datastructure;
+
+import java.util.HashMap;
+
+public class StackByArray {
+    int[] stack = null;
+    int count;
+    int cap;
+    private static final int DEFAULT_CAP = 10;
+
+    public StackByArray() {
+        this(DEFAULT_CAP);
+    }
+
+    public StackByArray(int cap) {
+        if (cap <= 0) {
+            this.cap = DEFAULT_CAP;
+        }
+        this.cap = cap;
+        this.count = 0;
+        this.stack = new int[cap];
+    }
+
+    public boolean push(int val) {
+        if (count < stack.length) {
+            stack[count++] = val;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int pop() {
+        if (count == 0) {
+            return -1;
+        }
+
+        return stack[--count];
+    }
+
+    public boolean empty() {
+        return count == 0;
+    }
+
+
+    public static void main(String[] args) {
+        StackByArray stack = new StackByArray();
+        for (int i=1; i < 12; i++) {
+            stack.push(i);
+        }
+
+        while (!stack.empty()) {
+            System.out.printf("%2d\t", stack.pop());
+        }
+        System.out.println();
+    }
+}
