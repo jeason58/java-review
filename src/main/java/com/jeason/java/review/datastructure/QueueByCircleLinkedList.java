1422433acc5db68ff693e592ff8821cb06e72c0c
wangzhixin <wangzhixin@megvii.com>
2020-04-29 15:34:01 +0800

    单项循环链表实现队列

--- src/main/java/com/jeason/java/review/datastructure/QueueByCircleLinkedList.java
+++ src/main/java/com/jeason/java/review/datastructure/QueueByCircleLinkedList.java
@@ -3,13 +3,29 @@ package com.jeason.java.review.datastructure;
 
 /**
  * 利用循环单链表实现一个队列
- * todo：测试有问题，待修复
+ *
+ * 思想：
+ * Queue内部是一个循环单链表，head为队列头指针，tail为队列尾指针
+ *
+ * 1) 初始状态下：head指向链表头，tail为null
+ * 2) 入队操作：
+ *   - 首先判断tail是否null，若tail=null，表明队列为空，将tail=head；
+ *   - 否则判断tail.next是否为head，若tail.next=head，表明队列已满，则新增节点；
+ *   - 否则，tail = tail.next; tail.value = newValue; （尾指针后移，并且赋值为入队的新值）
+ *
+ * 3) 出队操作：
+ *   - 若队列已空，直接返回-1；
+ *   - 将当前head.value返回，并且判断head是否==tail，若head==tail，则表明当前出队元素是最后一个元素，将tail赋值为null；
+ *   - 否则，head=head.next（头指针后移）
+ *
+ *
+ * 4) 判断队列是否已满：tail.next == head ?
+ * 5) 判断队列是否已空：tail == null ?
  * */
 
 public class QueueByCircleLinkedList {
     private final int DEFAULT_CAP = 10;
     private int cap;
-    private Node root;
     private Node head, tail;
 
     static class Node {
@@ -34,21 +50,15 @@ public class QueueByCircleLinkedList {
     }
 
     private void lazyInit() {
-        if (root != null) {
+        if (head != null) {
             return;
         }
 
-        Node p = root;
+        Node p = (head = new Node(0));
         for (int i = 0; i < this.cap; i++) {
-            if (p == null) {
-                root = p = new Node(0);
-            } else {
-                p.next = new Node(0);
-                p = p.next;
-            }
+            p.next = new Node(0);
+            p = p.next;
         }
-
-        head = root;
     }
 
     /**
@@ -58,7 +68,7 @@ public class QueueByCircleLinkedList {
      * 出队操作：head=head.next
      * */
     public boolean empty() {
-        return (tail == null) || (tail == head && head == root);
+        return (tail == null);
     }
 
     public boolean full() {
@@ -66,11 +76,11 @@ public class QueueByCircleLinkedList {
     }
 
     public synchronized void offer(int n) {
-        if (root == null) {
+        if (head == null) {
             lazyInit();
         }
 
-        if (tail == null) {
+        if (empty()) {
             tail = head;
             tail.value = n;
         } else if (full()) {
@@ -91,7 +101,12 @@ public class QueueByCircleLinkedList {
         }
 
         int res = head.value;
-        head = head.next;
+
+        if (head == tail) {
+            tail = null;
+        } else {
+            head = head.next;
+        }
 
         return res;
     }
@@ -103,6 +118,7 @@ public class QueueByCircleLinkedList {
             for (int i = 1; i <= 100; i++) {
                 try {
                     queue.offer(i);
+                    System.out.printf("Thread1 - 执行入队: %2d\n", i);
                     Thread.sleep(500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -118,8 +134,7 @@ public class QueueByCircleLinkedList {
                         Thread.sleep(500);
                         continue;
                     }
-                    System.out.printf("Thread2 poll: %2d\n", n);
-                    Thread.sleep(400);
+                    System.out.printf("Thread2 - 执行出队: %2d\n", n);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
